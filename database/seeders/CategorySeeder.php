976ee33fb22d4e9cd81c88a90de85241f5877519
Felix <felixsavero78@gmail.com>
2024-07-03 16:45:05 +0700

    feat: resource

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +14,11 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Category::create([
+            'name' => 'Food'
+        ]);
+        Category::create([
+            'name' => 'Gadget'
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/api/categories/{id}', function ($id) {
+    $category = Category::findOrFail($id);
+    return new CategoryResource($category);
 });
